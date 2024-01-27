785dd7d131a8db7af5c5527f90ac820bf8b7bfea
Kevin Aguirre <kevinaguirre463@gmail.com>
2024-01-27 13:05:05 -0500

    Setup .env

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
+require('dotenv').config({ path: '../.env'})
 const express = require('express')
 const app = express()
-require('dotenv').config()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
@@ -10,6 +10,7 @@ const bcrypt = require('bcryptjs')
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect(uri)
+mongoose.connect(process.env.URI)
     .then(() => console.log("connected"))
     .catch((e) => console.log(e))
+    
